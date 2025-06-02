cac6e182c2ec29f2677a3f8c05a13947a8a9a9e3
Shubhangi <raghuvanshishubhangi8@gmail.com>
2025-06-02 23:04:13 +0530

    Updated

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ function App() {
     <>
       <Routes>
         <Route path="/login" element={<Login />} />
+             <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route 
           path="/dashboard" 
